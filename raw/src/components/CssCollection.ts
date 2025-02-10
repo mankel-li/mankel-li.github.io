bec20ebff0b83553080f2fe48e47a2bd0384b58b
Mankel <mankel.li.ma@outlook.com>
2025-02-10 19:46:26 +0800

    .

--- raw/src/components/CssCollection.ts
+++ raw/src/components/CssCollection.ts
@@ -17,18 +17,58 @@ export const CssCollections: CssCollection[] = [
     background: 'background: url("/mk-bg.png") no-repeat center center / cover;',
   },
   {
-    label: '网格布局',
-    dom: `<div class="grid grid-cols-3 gap-2">
-                <div class="w-8 h-8 bg-white rounded-lg"></div>
-                <div class="w-8 h-8 bg-white rounded-lg"></div>
-                <div class="w-8 h-8 bg-white rounded-lg"></div>
-                <div class="w-8 h-8 bg-white rounded-lg"></div>
-                <div class="w-8 h-8 bg-white rounded-lg"></div>
-                <div class="w-8 h-8 bg-white rounded-lg"></div>
-                <div class="w-8 h-8 bg-white rounded-lg"></div>
-                <div class="w-8 h-8 bg-white rounded-lg"></div>
-                <div class="w-8 h-8 bg-white rounded-lg"></div>
-              </div>`,
-    css: '',
+    label: 'flex向右浮动',
+    dom: `<div class="mk-flex"><div class="w-16 h-16 bg-white rounded-lg mk-flex__item"></div><div class="w-16 h-16 bg-white rounded-lg mk-flex__item"></div><div class="w-16 h-16 bg-white rounded-lg mk-flex__item"></div></div>`,
+    css: '.mk-flex {display: flex; width: 80%; gap: 8px; div:nth-child(2) {margin-left: auto;}}',
+  },
+  {
+    label: '浮雕风格',
+    dom: `<div class="flex mk-emboss"><input placeholder="Input Your Text" /><button>S</button></div>`,
+    css: `.mk-emboss {
+      --ruler: 16px;
+      --color-bg: #ebecf0;
+      --color-white: #fff;
+      --color-shadow: #babecc;
+      gap: 12px;
+      button, input {
+        flex: auto;
+        border: 0;
+        outline: 0;
+        font-size: var(--ruler);
+        border-radius: 12px;
+        padding: var(--ruler);
+        background-color: var(--color-bg);
+        text-shadow: 1px 1px 0 var(--color-white);
+      }
+      input {
+        box-shadow: inset 2px 2px 5px var(--color-shadow), inset -5px -5px 10px var(--color-white);
+        width: 100%;
+        transition: all 0.2s ease-in-out;
+        appearance: none;
+        -webkit-appearance: none;
+      
+        &:focus {
+          box-shadow: inset 1px 1px 2px var(--color-shadow),
+            inset -1px -1px 2px var(--color-white);
+        }
+      }
+      button {
+        color: #61677c;
+        font-weight: bold;
+        box-shadow: -5px -5px 20px var(--color-white), 5px 5px 20px var(--color-shadow);
+        transition: all 0.2s ease-in-out;
+        cursor: pointer;
+        font-weight: 600;
+      
+        &:hover {
+          box-shadow: -2px -2px 5px var(--color-white), 2px 2px 5px var(--color-shadow);
+        }
+      
+        &:active {
+          box-shadow: inset 1px 1px 2px var(--color-shadow), inset -1px -1px 2px var(--color-white);
+        }
+      }
+    }`,
+    background: 'background: #ebecf0;',
   },
 ];
